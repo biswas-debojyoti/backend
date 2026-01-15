fa6c6f1c4a9c8c378d94def7bb5c4ec74504e015
OmmDigi <ommdigitalsolution@gmail.com>
2026-01-15 12:12:52 +0530

    backend

--- 3.1+Express+Server/3.1 Express Server/index.js
+++ 3.1+Express+Server/3.1 Express Server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
 const app = express();
 const port = 3000;
+const hostname = "localhost";
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.send("heeelow debu");
+});
+
+app.get("/debu", (req, res) => {
+  res.send("<h1>Hellow Debu</h1>");
+});
+
+app.get("/debus", (req, res) => {
+  res.send("<h1>Hellow Debu debus</h1>");
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running on port ${port}.`);
 });
